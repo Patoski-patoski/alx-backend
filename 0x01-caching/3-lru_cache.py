26f58fdedad8f0a1c2172dba1dbe40f9e7e51897
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-25 04:49:44 +0000

    a class LRUCache that inherits from BaseCaching and is a caching system

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -2,36 +2,59 @@
 """LRU caching
 """
 
+from typing import Dict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """LRU cache
-    """
+def delete_first_elem(d: Dict):
+    """deletes the first element in a dictionary
 
+    Args:
+        d (_type_): dictionary
+    """
+    if d:
+        first_key = next(iter(d))
+        del d[first_key]
+        print(f"DISCARD: {first_key}")
+
+
+class LRUCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system:
+    Args:
+        BaseCaching (_type_): Base class
+    """
     def __init__(self):
-        """Constructor
+        """constructor
         """
         super().__init__()
-        self.keys = []
 
     def put(self, key, item):
-        """Add an item in the cache
+        """assign to the dictionary self.cache_data the item value for the key.
+        Args:
+            key (_type_): key
+            item (_type_): value
         """
+        store_values = []
+
         if key is not None and item is not None:
+            store_values.append(item)
+            if key in self.cache_data.keys():
+                del self.cache_data[key]
+
             self.cache_data[key] = item
-            if key in self.keys:
-                self.keys.remove(key)
-            self.keys.append(key)
-            if len(self.keys) > BaseCaching.MAX_ITEMS:
-                first = self.keys.pop(0)
-                del self.cache_data[first]
-                print("DISCARD: {}".format(first))
+
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                delete_first_elem(self.cache_data)
 
     def get(self, key):
         """Get an item by key
+        Args:
+            key (_type_): key item
+        Returns:
+            _type_: return the value in self.cache_data linked to key.
         """
-        if key in self.keys:
-            self.keys.remove(key)
-            self.keys.append(key)
+        if key in self.cache_data.keys():
+            item = self.cache_data[key]
+            del self.cache_data[key]
+            self.cache_data[key] = item
         return self.cache_data.get(key, None)
