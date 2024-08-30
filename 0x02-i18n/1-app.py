984b6e855819a9b8e0693ab6268bdd4d514cd8dd
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-30 22:52:38 +0000

    instantiate the Babel object

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,17 +4,23 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+class Config:
+    """config for your Flask app."""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    
+
+app.config.from_object(Config)
 
 babel = Babel(app)
-
 
 @app.route('/')
 def index():
     """index file"""
     return render_template('1-index.html')
-
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
