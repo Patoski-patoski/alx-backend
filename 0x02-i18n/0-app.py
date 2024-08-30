5a31dd7beb5b24e2953260e3700938ddc7215538
Okafor Patrick C <patrickpeters911@gmail.com>
2024-08-30 21:24:57 +0000

    setup a basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """index file"""
+    return render_template('0-index.html')
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
