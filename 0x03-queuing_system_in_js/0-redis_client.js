8129ed3d12ed04b410caa74e52a659b1510be07e
Okafor Patrick C <patrickpeters911@gmail.com>
2024-09-28 07:02:23 +0000

    Starting a Node Redis Client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -4,5 +4,5 @@ const err_message = 'Redis client not connected to the server: ERROR_MESSAGE: ';
 const connect_message = 'Redis client connected to the server';
 
 const client = createClient();
-client.on('error', err => console.log(err_message, err_message));
+client.on('error', err => console.log(err_message, err));
 client.on('connect', () => console.log(connect_message));
